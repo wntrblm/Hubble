113ad3dd3caedb07821f8e55a78ba72c932b99a7
Thea Flowers <me@thea.codes>
2021-11-12 16:02:41 -0500

    Remove ADG1414 drivers

--- firmware/src/drivers/stel_adg1414.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "stel_sercom_spi.h"
-
-void stel_adg1414_init(const struct StelSERCOMSPI* spi);
-void stel_adg1414_write(const uint8_t data[], size_t count);
-void stel_adg1414_write_switches(const bool data[], const size_t count);
