836a8ebc99818bac16915cce44d4dddb179c7793
Thea Flowers <me@thea.codes>
2021-12-18 14:24:30 -0500

    Add __main__ that drops into IPython shell

---
+++ python_library/hubble/__main__.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2021 Alethea Katherine Flowers.
+# Published under the standard MIT License.
+# Full text available at: https://opensource.org/licenses/MIT
+
+import IPython
+
+from hubble.hubble import Hubble
+
+hubble = Hubble()
+
+IPython.embed()
